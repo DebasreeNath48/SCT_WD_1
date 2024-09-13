c4dfbdfaedaace5d8641cda52aeb533c96bce441
Debasree Nath <24032976-DebasreeNath@users.noreply.replit.com>
2024-09-13 06:30:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+const navbar = document.getElementById('navbar');
+
+window.onscroll = function() {
+  if (window.scrollY >= 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+};
